aa29acd0e6703331fe9b7f484e02b7bc1e78c5db
Arijit <arijit123roy098@gmail.com>
2025-05-31 17:35:45 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PeniChat",
+  description: "This website is for my personal use, not for public",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logo/penichat.svg" type="image/x-icon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,59 @@
+import * as React from "react"
+import { Github, MessagesSquare } from "lucide-react"
+import Link from "next/link"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { ThreadList } from "./assistant-ui/thread-list"
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="https://github.com/arijiiiitttt/penichat" target="_blank">
+                  <img src="/logo/penichat.svg" alt="Icon" className="lg:size-10 md:size-10 size-12" />
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold text-md">PeniChat</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <ThreadList />
+      </SidebarContent>
+
+      <SidebarRail />
+      <SidebarFooter>
+        <SidebarMenu>
+
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="https://github.com/arijiiiitttt/chatx" target="_blank">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <Github className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">GitHub</span>
+                  <span className="">View Source Code</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
+
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
